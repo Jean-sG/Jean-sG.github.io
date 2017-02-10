034ca7f080fbfe52c80a180571fb72a8c792d860
Jean-sG <jeansh.guillaume@gmail.com>
2017-02-10 17:41:31 +0100

    add js

--- js/0210.js
+++ js/0210.js
@@ -1,10 +1,27 @@
-var xmlhttp = new XMLHttpRequest();
+function requeteApiWS(monapiURL , elementiD) 
+{var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("demo").innerHTML = jsonPretty;
+        document.getElementById("elementID").innerHTML = jsonPretty;
     }
 };
-xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
+xmlhttp.open("GET", "monapiURL", true);
 xmlhttp.send();
+}
+
+
+function homePageLoading() {
+    url = 'http://bitcoin.mubiz.com/blockchaininfo';
+    elementID = 'blockchaininfo';
+    requeteApiWS(url, elementID);
+    
+        url = 'http://bitcoin.mubiz.com/mininginfo ';
+    elementID = 'mininginfo';
+    requeteApiWS(url, elementID);
+    
+        url = 'http://bitcoin.mubiz.com/peerinfo';
+    elementID = 'peerinfo';
+    requeteApiWS(url, elementID);
+}
