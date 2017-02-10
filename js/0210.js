1fb88b3b43fcee55b6dc9937cd9b134a2729c8a9
Jean-sG <jeansh.guillaume@gmail.com>
2017-02-10 18:32:49 +0100

    modif

--- js/0210.js
+++ js/0210.js
@@ -1,27 +1,32 @@
-function requeteApiWS(monapiURL , elementiD) 
-{var xmlhttp = new XMLHttpRequest();
+function requeteApiWS(monapiURL, elementiD){
+    var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById("elementID").innerHTML = jsonPretty;
+        document.getElementById(elementiD).innerHTML = jsonPretty;
     }
 };
-xmlhttp.open("GET", "monapiURL", true);
+xmlhttp.open("GET", monapiURL, true);
 xmlhttp.send();
 }
 
 
 function homePageLoading() {
-    url = 'http://bitcoin.mubiz.com/blockchaininfo';
-    elementID = 'blockchaininfo';
-    requeteApiWS(url, elementID);
     
-        url = 'http://bitcoin.mubiz.com/mininginfo ';
-    elementID = 'mininginfo';
-    requeteApiWS(url, elementID);
+    url = 'http://bitcoin.mubiz.com/info';
+    element = 'info';
+    requeteApiWS(url, element);
+    
+    url = 'http://bitcoin.mubiz.com/blockchaininfo';
+    element = 'blockchaininfo';
+    requeteApiWS(url, element);
+    
+        url = 'http://bitcoin.mubiz.com/mininginfo';
+    element = 'mininginfo';
+    requeteApiWS(url, element);
     
         url = 'http://bitcoin.mubiz.com/peerinfo';
-    elementID = 'peerinfo';
-    requeteApiWS(url, elementID);
+    element = 'peerinfo';
+    requeteApiWS(url, element);
 }
