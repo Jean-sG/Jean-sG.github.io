f7825703ae0c2b2a52fb6c06c1ca7d1e440c83b6
Jean-sG <jeansh.guillaume@gmail.com>
2017-04-07 14:43:05 +0200

    mise en place des formulaires

--- js/form.js
+++ js/form.js
@@ -1,24 +1,45 @@
-function requeteApiWS(monapiURL, elementiD){
-    var xmlhttp = new XMLHttpRequest();
+function byAdress() {
+    var x;
+
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("numb").value;
+if (isURL(x)){
+    requeteAPI(x);
+}
+        }
+
+function isURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+ if(!pattern.test(str)) {alert("Merci d'entrer un URL");}
+  return pattern.test(str);
+}
+
+
+
+function byFile() {
+    var x, text, allurl;
+    // Get the value of the input field with id="numb"
+    x = document.getElementById("fileChain").value;
+    text = "https://bitcoin.mubiz.com/";
+    allurl = text.concat(x);
+    requeteAPI(allurl);
+        }
+
+function requeteAPI(monapiURL){
+    
+        var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById(elementiD).innerHTML = jsonPretty;
+        document.getElementById("resultat").innerHTML = jsonPretty;
     }
 };
 xmlhttp.open("GET", monapiURL, true);
 xmlhttp.send();
-}
-
-document.getElementById("boutonAdress").onclick = function() {byAdress()};
-
-
-function byAdress() {
-     var inpObj = document.getElementById("InputAdress");
-    if (inpObj.checkValidity() == false) {
-        document.getElementById("resultat").innerHTML = inpObj.validationMessage;
-    } else {
-       requeteApiWS(inpObj, resultat);
-    } 
 }
