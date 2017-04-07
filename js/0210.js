31e5e7915bf2de9f39495459d16ebb7dac974d16
Jean-sG <jeansh.guillaume@gmail.com>
2017-04-07 16:01:06 +0200

    finalisation

--- js/0210.js
+++ js/0210.js
@@ -4,7 +4,7 @@ xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var myObj = this.responseText;
         var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
-        document.getElementById(elementiD).innerHTML = jsonPretty;
+        document.getElementById(elementiD).innerHTML = syntaxHighlight(jsonPretty);
     }
 };
 xmlhttp.open("GET", monapiURL, true);
@@ -29,4 +29,24 @@ function homePageLoading() {
         url = 'https://bitcoin.mubiz.com/peerinfo';
     element = 'peerinfo';
     requeteApiWS(url, element);
+}
+
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
 }
